e1a514ed87e98f913d565e6cd5d728fb554f1e12
Ply_py <me@plypy.com>
2014-05-20 20:04:32 +0800

    refactoring & cleaning trial run

---
+++ app/routes/main.js
@@ -0,0 +1,27 @@
+var https = require('https');
+var app = require(global.__commonModule).app;
+
+// LOGIN-LOGOUT
+app.get('/', function(req, res, next) {
+
+  if (!req.session.user) {
+    return next();
+  }
+
+  https.get({
+    host: 'answers.openmrs.org',
+    path: '/users/' + req.session.user.uid
+  }, function(response) {
+    if (response.statusCode === 200) {
+      res.locals({
+        osqaUser: true
+      });
+    } else {
+      res.locals({
+        osqaUser: false
+      });
+    }
+
+    res.render('root');
+  });
+});
