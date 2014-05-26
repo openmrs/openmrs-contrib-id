f89aace53722c66edb140fc265bdfbf9fde91741
Ply_py <me@plypy.com>
2014-05-26 13:54:33 +0800

    cleanup console.log

--- app/routes/resources.js
+++ app/routes/resources.js
@@ -4,7 +4,5 @@ var app = require(global.__commonModule).app;
 app.get('/resource/*', function(req, res, next) {
   // Need to be change
   var resourcePath = path.join(__dirname, '/../../resource/', req.params[0]);
-  console.log(__dirname);
-  console.log(resourcePath);
   res.sendfile(resourcePath);
 });
