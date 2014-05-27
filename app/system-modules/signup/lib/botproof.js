ddf9c96fac0225634d4fe5a23e983b9b87e116a4
Ply_py <me@plypy.com>
2014-05-27 23:36:11 +0800

    slight indentation cleanup

--- app/system-modules/signup/lib/botproof.js
+++ app/system-modules/signup/lib/botproof.js
@@ -1,27 +1,28 @@
-var crypto = require('crypto')
-,   Common = require(global.__commonModule)
-,   app = Common.app
-,   log = Common.logger.add('botproof')
-,   signupConf = require('../conf.signup.json')
-,   async = require('async')
-,   _ = require('underscore')
-,   dns = require('dns')
-,   db = Common.db
-;
+var crypto = require('crypto'),
+  Common = require(global.__commonModule),
+  app = Common.app,
+  log = Common.logger.add('botproof'),
+  signupConf = require('../conf.signup.json'),
+  async = require('async'),
+  _ = require('underscore'),
+  dns = require('dns'),
+  db = Common.db;
 
 db.define('IPWhitelist', {
-    id: {type: db.INTEGER, primaryKey: true, autoIncrement: true},
-    address: db.STRING
+  id: {
+    type: db.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  address: db.STRING
 })
 
 var SECRET = crypto.createHash('sha1').update(Math.random().toString())
-             .digest('hex');
+  .digest('hex');
 
 function ip(req) {
-    var xf = req.header('X-Forwarded-For') || '';
-    return xf
-        ? xf.split(/ *, */)[0]
-        : req.connection.remoteAddress;
+  var xf = req.header('X-Forwarded-For') || '';
+  return xf ? xf.split(/ *, */)[0] : req.connection.remoteAddress;
 }
 
 function reverseIp(req) {
@@ -31,200 +32,200 @@ function reverseIp(req) {
 }
 
 function badRequest(next, optionalMessage) {
-    var err = new Error(optionalMessage ||'Submitted form failed '+
-        'anti-bot checking.');
-    err.statusCode = 400;
-    next(err);
+  var err = new Error(optionalMessage || 'Submitted form failed ' +
+    'anti-bot checking.');
+  err.statusCode = 400;
+  next(err);
 }
 
 function hashField(name, spin) {
-    // Disguise a legitimate field name (like "firstname") with an
-    // obfuscated hash name based on this request's spinner.
+  // Disguise a legitimate field name (like "firstname") with an
+  // obfuscated hash name based on this request's spinner.
 
-    var hash = crypto.createHash('md5');
-    hash.update(name).update(spin).update(SECRET);
-    log.trace('diguised field with name "'+name+'", spinner "'+spin+'"');
-    return hash.digest('hex');
+  var hash = crypto.createHash('md5');
+  hash.update(name).update(spin).update(SECRET);
+  log.trace('diguised field with name "' + name + '", spinner "' + spin + '"');
+  return hash.digest('hex');
 }
 
 // Expose stuff.
 app.helpers({
-    disguise: hashField
+  disguise: hashField
 })
 
 module.exports = {
-    // Every method in here is Connect middleware, and is used by chaining it to
-    // the Express router. `botproof.generators` and `botproof.parsers` can also
-    // be used to invoke all methods, as defined below.
+  // Every method in here is Connect middleware, and is used by chaining it to
+  // the Express router. `botproof.generators` and `botproof.parsers` can also
+  // be used to invoke all methods, as defined below.
 
-    generateTimestamp: function generateTimestamp(req, res, next) {
-        var timestamp = Date.now(),
-            cipher = crypto.createCipher('aes192', SECRET)
+  generateTimestamp: function generateTimestamp(req, res, next) {
+    var timestamp = Date.now(),
+      cipher = crypto.createCipher('aes192', SECRET)
 
-        cipher.update(timestamp.toString())
+    cipher.update(timestamp.toString())
 
-        res.local('timestamp', cipher.final('hex'))
-        next();
-    },
+    res.local('timestamp', cipher.final('hex'))
+    next();
+  },
 
-    checkTimestamp: function checkTimestamp(req, res, next) {
-        if (!req.body.timestamp) return badRequest(next);
+  checkTimestamp: function checkTimestamp(req, res, next) {
+    if (!req.body.timestamp) return badRequest(next);
 
-        // Decipher
-        var decipher = crypto.createDecipher('aes192', SECRET)
+    // Decipher
+    var decipher = crypto.createDecipher('aes192', SECRET)
 
-        decipher.update(req.body.timestamp, 'hex')
-        var timestamp = decipher.final('utf8');
+    decipher.update(req.body.timestamp, 'hex')
+    var timestamp = decipher.final('utf8');
 
 
-        var then = new Date(parseInt(timestamp, 10))
-        ,   now = new Date(Date.now());
+    var then = new Date(parseInt(timestamp, 10)),
+      now = new Date(Date.now());
 
-        // Throw out malformed timestamps
-        if (isNaN(then.valueOf())) return badRequest(next);
+    // Throw out malformed timestamps
+    if (isNaN(then.valueOf())) return badRequest(next);
 
-        var diff = now - then
-        ,   minimumTime = signupConf.requiredSubmitTimeSec * 1000;
-        log.trace('submission time difference: '+diff);
+    var diff = now - then,
+      minimumTime = signupConf.requiredSubmitTimeSec * 1000;
+    log.trace('submission time difference: ' + diff);
 
-        // Throw out a time in the future or too far in the past.
-        if (diff < 0) {
-            return badRequest(next, 'Submitted form received from a time in the'+
-                ' future');
-        } else if (diff > signupConf.signupFormMaxAgeHours *60*60*1000) {
-            return badRequest(next, '')
-        }
+    // Throw out a time in the future or too far in the past.
+    if (diff < 0) {
+      return badRequest(next, 'Submitted form received from a time in the' +
+        ' future');
+    } else if (diff > signupConf.signupFormMaxAgeHours * 60 * 60 * 1000) {
+      return badRequest(next, '')
+    }
 
-        // Delay the submission if it was completed too soon
-        if (diff < minimumTime) {
-            log.info('deferring submission received in '+diff+' ms from '+
-                ip(req));
-            return setTimeout(next, minimumTime - diff);
-        }
+    // Delay the submission if it was completed too soon
+    if (diff < minimumTime) {
+      log.info('deferring submission received in ' + diff + ' ms from ' +
+        ip(req));
+      return setTimeout(next, minimumTime - diff);
+    }
 
-        next();
-    },
+    next();
+  },
 
-    generateSpinner: function generateSpinner(req, res, next) {
-        // The spinner is a hash of the current time, the client's IP, and the
-        // secret. It's a hidden field within the page.
+  generateSpinner: function generateSpinner(req, res, next) {
+    // The spinner is a hash of the current time, the client's IP, and the
+    // secret. It's a hidden field within the page.
 
-        // Generate the spinner and attach it to the request.
-        var timestamp = res.local('timestamp')
-        ,   hash = crypto.createHash('md5')
-        ;
+    // Generate the spinner and attach it to the request.
+    var timestamp = res.local('timestamp'),
+      hash = crypto.createHash('md5');
 
-        log.trace('generating spinner with timestamp "'+timestamp+'" for '+
-            'ip address "'+ip(req)+'"');
+    log.trace('generating spinner with timestamp "' + timestamp + '" for ' +
+      'ip address "' + ip(req) + '"');
 
-        hash.update(timestamp.toString())
-            .update(ip(req).toString())
-            .update(SECRET);
-        var spin = hash.digest('hex');
+    hash.update(timestamp.toString())
+      .update(ip(req).toString())
+      .update(SECRET);
+    var spin = hash.digest('hex');
 
-        res.local('spinner', spin);
-        next();
-    },
+    res.local('spinner', spin);
+    next();
+  },
 
-    unscrambleFields: function unscrambleFields(req, res, next) {
-        // Parse through the body and unscramble any fields.
+  unscrambleFields: function unscrambleFields(req, res, next) {
+    // Parse through the body and unscramble any fields.
 
-        // Fail the request if no spinner was passed
-        if (!req.body.spinner) return badRequest(next);
+    // Fail the request if no spinner was passed
+    if (!req.body.spinner) return badRequest(next);
 
-        var expected = signupConf.signupFieldNames;
-        expected.push(signupConf.honeypotFieldName); // also look for honeypot
+    var expected = signupConf.signupFieldNames;
+    expected.push(signupConf.honeypotFieldName); // also look for honeypot
 
-        var spin = req.body.spinner
-        ,   result = {}
-        ;
+    var spin = req.body.spinner,
+      result = {};
 
-        for (var i in expected) {
-            // Determine the field's hash, and set its value on the unscrambled
-            // side.
-            var f = expected[i]
-            ,   hashed = hashField(f, spin);
+    for (var i in expected) {
+      // Determine the field's hash, and set its value on the unscrambled
+      // side.
+      var f = expected[i],
+        hashed = hashField(f, spin);
 
-            if (req.body[hashed]) {
-                result[f] = req.body[hashed] || '';
-                log.trace('unscrambled field "'+f+'"='+req.body[hashed])
-            }
-        }
+      if (req.body[hashed]) {
+        result[f] = req.body[hashed] || '';
+        log.trace('unscrambled field "' + f + '"=' + req.body[hashed])
+      }
+    }
 
-        // Patch the captcha challenge field over to our results. The field
-        // "recaptcha_challenge_field" is inserted by JavaScript as Recaptcha
-        // is loaded, so we are unable to hash it. Since this field is inserted
-        // dynamically, it is still relatively bot-proof.
-        var rcf = 'recaptcha_challenge_field';
-        if (req.body[rcf]) result[rcf] = req.body[rcf];
+    // Patch the captcha challenge field over to our results. The field
+    // "recaptcha_challenge_field" is inserted by JavaScript as Recaptcha
+    // is loaded, so we are unable to hash it. Since this field is inserted
+    // dynamically, it is still relatively bot-proof.
+    var rcf = 'recaptcha_challenge_field';
+    if (req.body[rcf]) result[rcf] = req.body[rcf];
 
-        // Replace the body with the un-hashed results.
-        req.body = result;
+    // Replace the body with the un-hashed results.
+    req.body = result;
 
-        next();
-    },
+    next();
+  },
 
-    // Invalidate the request if the honeypot has been filled (presumably by a
-    // bot). Honeypot field name is configured in conf.signup.js
-    checkHoneypot: function checkHoneypot(req, res, next) {
-        if (req.body[signupConf.honeypotFieldName])
-            return badRequest(next);
+  // Invalidate the request if the honeypot has been filled (presumably by a
+  // bot). Honeypot field name is configured in conf.signup.js
+  checkHoneypot: function checkHoneypot(req, res, next) {
+    if (req.body[signupConf.honeypotFieldName])
+      return badRequest(next);
 
-        next();
-    },
+    next();
+  },
 
-    spamListLookup: function spamListLookup(req, res, next) {
-      var rev = reverseIp(req)
-      ,   spams = signupConf.dnsSpamLists
+  spamListLookup: function spamListLookup(req, res, next) {
+    var rev = reverseIp(req),
+      spams = signupConf.dnsSpamLists
 
-      // check the address with each list
-      async.map(Object.keys(spams), function (list, cb) {
-        dns.lookup(rev + '.' + list, function(err, address) {
-          if (err) {
-            if (err.code === 'ENOTFOUND') cb(null, false) // address not on list
-            else cb(err)
+    // check the address with each list
+    async.map(Object.keys(spams), function(list, cb) {
+      dns.lookup(rev + '.' + list, function(err, address) {
+        if (err) {
+          if (err.code === 'ENOTFOUND') cb(null, false) // address not on list
+          else cb(err)
 
-          } else if (_.contains(spams[list].returnCodes, address)) {
+        } else if (_.contains(spams[list].returnCodes, address)) {
           // address IS on list and proper return code specified
 
-            db.find('IPWhitelist', {address: ip(req)}, function(err, insts) {
-                if (insts.length) // address is whitelisted
-                    cb(null, false)
-                else
-                    cb(null, true)
-            })
+          db.find('IPWhitelist', {
+            address: ip(req)
+          }, function(err, insts) {
+            if (insts.length) // address is whitelisted
+              cb(null, false)
+            else
+              cb(null, true)
+          })
 
-          } else {
-            cb(null, false)
-          }
-        })
-
-      }, function callback(err, results) {
-        if (err) return next(err);
-
-        if (_.contains(results, true)) { // if this address was indicated as spam
-          log.info('IP address ' + ip(req) + ' flagged as spam')
-          return badRequest(next, "Your IP address, "+ip(req)+", was "+
-          "flagged as a spam address by our spam-blocking lists. Please open an "+
-          "issue if you believe this is in error.");
+        } else {
+          cb(null, false)
         }
-
-        next(); // not spam!
       })
-    }
+
+    }, function callback(err, results) {
+      if (err) return next(err);
+
+      if (_.contains(results, true)) { // if this address was indicated as spam
+        log.info('IP address ' + ip(req) + ' flagged as spam')
+        return badRequest(next, "Your IP address, " + ip(req) + ", was " +
+          "flagged as a spam address by our spam-blocking lists. Please open an " +
+          "issue if you believe this is in error.");
+      }
+
+      next(); // not spam!
+    })
+  }
 }
 
 module.exports.SECRET = SECRET // used by testing
 
 
 module.exports.generators = [
-    module.exports.generateTimestamp,
-    module.exports.generateSpinner
+  module.exports.generateTimestamp,
+  module.exports.generateSpinner
 ];
 
 module.exports.parsers = [
-    module.exports.unscrambleFields,
-    module.exports.checkTimestamp,
-    module.exports.checkHoneypot,
-    module.exports.spamListLookup
+  module.exports.unscrambleFields,
+  module.exports.checkTimestamp,
+  module.exports.checkHoneypot,
+  module.exports.spamListLookup
 ];
--- app/system-modules/signup/lib/signup.js
+++ app/system-modules/signup/lib/signup.js
@@ -1,30 +1,30 @@
 var url = require('url'),
-	path = require('path'),
-	botproof = require('./botproof'),
-	signupMiddleware = require('./middleware');
+  path = require('path'),
+  botproof = require('./botproof'),
+  signupMiddleware = require('./middleware');
 
 var Common = require(global.__commonModule),
-	conf = Common.conf,
-	app = Common.app,
-	ldap = Common.ldap,
-	log = Common.logger.add('signup'),
-	mid = Common.mid,
-	validate = Common.validate,
-	verification = Common.verification,
-	nav = Common.userNav;
+  conf = Common.conf,
+  app = Common.app,
+  ldap = Common.ldap,
+  log = Common.logger.add('signup'),
+  mid = Common.mid,
+  validate = Common.validate,
+  verification = Common.verification,
+  nav = Common.userNav;
 
 /*
 USER-NAV
 ========
 */
 nav.add({
-	"name": "Sign Up",
-	"url": "/signup",
-	"viewName": "signup",
-	"visibleLoggedOut": true,
-	"visibleLoggedIn": false,
-	"icon": "icon-asterisk",
-	"order": 20
+  "name": "Sign Up",
+  "url": "/signup",
+  "viewName": "signup",
+  "visibleLoggedOut": true,
+  "visibleLoggedIn": false,
+  "icon": "icon-asterisk",
+  "order": 20
 });
 
 /*
@@ -33,150 +33,159 @@ ROUTES
 */
 // get signup from /signup or from / and handle accordingly
 app.get(/^\/signup\/?$|^\/$/i, validate.receive(), botproof.generators,
-	function(req, res, next){
-		if (req.session.user) return next(); // pass onward if a user is signed in
+  function(req, res, next) {
+    if (req.session.user) return next(); // pass onward if a user is signed in
 
-		// parse querystrings for pre-populated data
-		var values = res.local('validation').values || {}, renderLayout = true;
-		var query = url.parse(req.url, true).query
+    // parse querystrings for pre-populated data
+    var values = res.local('validation').values || {},
+      renderLayout = true;
+    var query = url.parse(req.url, true).query
 
-		for (var prop in query) {
-			if (/^(firstname|lastname|username|email|)$/.test(prop))
-				values[prop] = query[prop];
-		}
+    for (var prop in query) {
+      if (/^(firstname|lastname|username|email|)$/.test(prop))
+        values[prop] = query[prop];
+    }
 
-		// handle layout query string & determine which view to render
-		renderLayout = (query.layout == 'false') ? false : true;
-		var viewPath = (renderLayout) ? __dirname+'/../views/signup' : __dirname+'/../views/signup-standalone';
+    // handle layout query string & determine which view to render
+    renderLayout = (query.layout == 'false') ? false : true;
+    var viewPath = (renderLayout) ? __dirname + '/../views/signup' : __dirname + '/../views/signup-standalone';
 
-		// render the page
-		res.render(viewPath, {
-			values: values,
-			layout: renderLayout,
-			renderLayout: renderLayout, // allows view to see whether or not it has layout
-			bodyAppend: '<script type="text/javascript" src="https://www.google.com/recaptcha/api/challenge?k='+conf.validation.recaptchaPublic+'"></script>'
-		});
-	});
+    // render the page
+    res.render(viewPath, {
+      values: values,
+      layout: renderLayout,
+      renderLayout: renderLayout, // allows view to see whether or not it has layout
+      bodyAppend: '<script type="text/javascript" src="https://www.google.com/recaptcha/api/challenge?k=' + conf.validation.recaptchaPublic + '"></script>'
+    });
+  });
 app.get('/signup', mid.forceLogout); // prevent from getting 404'd if a logged-in user hits /signup
 
 app.post('/signup', mid.forceLogout, botproof.parsers,
-	signupMiddleware.includeEmpties, validate(), function(req, res, next){
-		var id = req.body.username, first = req.body.firstname, last = req.body.lastname,
-			email = req.body.email, pass = req.body.password, captcha = req.body.recaptcha_response_field;
+  signupMiddleware.includeEmpties, validate(), function(req, res, next) {
+    var id = req.body.username,
+      first = req.body.firstname,
+      last = req.body.lastname,
+      email = req.body.email,
+      pass = req.body.password,
+      captcha = req.body.recaptcha_response_field;
 
-		if (!id || !first || !last || !email || !pass || !captcha) {
-			res.send('Unauthorized POST error', { 'Content-Type': 'text/plain' }, 403);
-			res.end();
-		}
+    if (!id || !first || !last || !email || !pass || !captcha) {
+      res.send('Unauthorized POST error', {
+        'Content-Type': 'text/plain'
+      }, 403);
+      res.end();
+    }
 
-		var id = id.toLowerCase();
+    var id = id.toLowerCase();
 
-		// will be called after account is created and validation process started
-		var finishCalls = 0, errored = false;
-		var finish = function(err){
-			if (err && errored == false) { // handle error
-				return next(err);
-				errored = true;
-			}
-			else {
-				finishCalls++;
-				if (finishCalls == 2) { // display welcome & verify notification
-					req.flash('success', "<p>Thanks and welcome to the OpenMRS Community!</p>"
-					+"<p>Before you can use your OpenMRS ID across our services, we need to verify your email address.</p>"
-					+"<p>We've sent an email to <strong>"+email+"</strong> with instructions to complete the signup process.</p>");
-					res.redirect('/signup/verify', 303);
-				}
-			}
-		}
+    // will be called after account is created and validation process started
+    var finishCalls = 0,
+      errored = false;
+    var finish = function(err) {
+      if (err && errored == false) { // handle error
+        return next(err);
+        errored = true;
+      } else {
+        finishCalls++;
+        if (finishCalls == 2) { // display welcome & verify notification
+          req.flash('success', "<p>Thanks and welcome to the OpenMRS Community!</p>" + "<p>Before you can use your OpenMRS ID across our services, we need to verify your email address.</p>" + "<p>We've sent an email to <strong>" + email + "</strong> with instructions to complete the signup process.</p>");
+          res.redirect('/signup/verify', 303);
+        }
+      }
+    }
 
-		// add the user to ldap
-		ldap.addUser(id, first, last, email, pass, function(e, userobj){
-			if (e) finish(e);
-			log.info('created account "'+id+'"');
+    // add the user to ldap
+    ldap.addUser(id, first, last, email, pass, function(e, userobj) {
+      if (e) finish(e);
+      log.info('created account "' + id + '"');
 
-			// lock out the account until it has been verified
-			ldap.lockoutUser(id, function(err){
-				if (err) finish(err);
-				finish();
-			});
-		});
+      // lock out the account until it has been verified
+      ldap.lockoutUser(id, function(err) {
+        if (err) finish(err);
+        finish();
+      });
+    });
 
-		// validate email before completing signup
-		verification.begin({
-			urlBase: 'signup',
-			email: email,
-			subject: '[OpenMRS] Welcome to the OpenMRS Community',
+    // validate email before completing signup
+    verification.begin({
+      urlBase: 'signup',
+      email: email,
+      subject: '[OpenMRS] Welcome to the OpenMRS Community',
       template: path.join(__dirname, '../views/welcome-verify-email.ejs'),
-			// template: path.relative(global.__apppath, __dirname+'/../views/welcome-verify-email.ejs'),
-			locals: {
-				displayName: first+' '+last,
-				username: id,
-				userCredentials: {
-					id: id,  email: email
-				}
-			},
-			timeout: 0
-		}, function(err){
-			if (err) finish(err);
+      // template: path.relative(global.__apppath, __dirname+'/../views/welcome-verify-email.ejs'),
+      locals: {
+        displayName: first + ' ' + last,
+        username: id,
+        userCredentials: {
+          id: id,
+          email: email
+        }
+      },
+      timeout: 0
+    }, function(err) {
+      if (err) finish(err);
 
-			finish();
-		});
-});
+      finish();
+    });
+  });
 
 app.get('/signup/verify', function(req, res, next) {
-	res.render('signedup');
+  res.render('signedup');
 });
 
 // verification
 app.get('/signup/:id', function(req, res, next) {
-	verification.check(req.params.id, function(err, valid, locals){
-		if (err) return next(err);
-		if (valid) {
-			var user = locals.userCredentials;
+  verification.check(req.params.id, function(err, valid, locals) {
+    if (err) return next(err);
+    if (valid) {
+      var user = locals.userCredentials;
 
-			// enable the account, allowing logins
-			ldap.enableUser(user.id, function(err, userobj){
-				if (err) return next(err);
-				log.debug(user.id+': account enabled');
-				verification.clear(req.params.id);
-				req.flash('success', 'Your account was successfully created. Welcome!');
+      // enable the account, allowing logins
+      ldap.enableUser(user.id, function(err, userobj) {
+        if (err) return next(err);
+        log.debug(user.id + ': account enabled');
+        verification.clear(req.params.id);
+        req.flash('success', 'Your account was successfully created. Welcome!');
 
-				req.session.user = userobj;
-				res.redirect('/');
+        req.session.user = userobj;
+        res.redirect('/');
 
-			});
-		}
-		else {
-			req.flash('error', 'The requested signup verification does not exist.');
-			res.redirect('/');
-		}
-	});
+      });
+    } else {
+      req.flash('error', 'The requested signup verification does not exist.');
+      res.redirect('/');
+    }
+  });
 });
 
 // AJAX, check whether or not user exists
-app.get('/checkuser/*', function(req, res, next){
-	if (req.isXMLHttpRequest) {
-		ldap.getUser(req.params[0], function(e, data){
-			if (e) {
-				if (e.message=='User data not found') res.end(JSON.stringify({exists: false}));
-				else if (e.message=='Illegal username specified') res.end(JSON.stringify({illegal: true}));
-			}
-			else if (data) res.end(JSON.stringify({exists: true}));
-			else next(e);
-		});
-	}
-	else res.redirect('/signup');
+app.get('/checkuser/*', function(req, res, next) {
+  if (req.isXMLHttpRequest) {
+    ldap.getUser(req.params[0], function(e, data) {
+      if (e) {
+        if (e.message == 'User data not found') res.end(JSON.stringify({
+          exists: false
+        }));
+        else if (e.message == 'Illegal username specified') res.end(JSON.stringify({
+          illegal: true
+        }));
+      } else if (data) res.end(JSON.stringify({
+        exists: true
+      }));
+      else next(e);
+    });
+  } else res.redirect('/signup');
 });
 
 
 // Resource handler
 app.get('/signup/resource/*', function(req, res, next) {
 
-	// resolve the path
-	var file = path.join(__dirname, '/../resource/', req.params[0]);
+  // resolve the path
+  var file = path.join(__dirname, '/../resource/', req.params[0]);
 
-	// transmit the file
-	res.sendfile(file, function(err){
-		if (err) return next(err);
-	});
+  // transmit the file
+  res.sendfile(file, function(err) {
+    if (err) return next(err);
+  });
 });
