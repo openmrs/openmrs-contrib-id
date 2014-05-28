82d8ca799f52331b99340b722ebc48839074ef1a
Elliott Williams <e@elliottwillia.ms>
2014-05-28 13:18:50 -0400

    Deleted resource route; it's now in environment.js

--- app/routes/resources.js
+++
@@ -1,8 +0,0 @@
-var path = require('path');
-var app = require(global.__commonModule).app;
-
-app.get('/resource/*', function(req, res, next) {
-  // Need to be change
-  var resourcePath = path.join(__dirname, '/../../resource/', req.params[0]);
-  res.sendfile(resourcePath);
-});
