e71569002fea07e47355c3467a474d4c5bccc78e
Elliott Williams <e@elliottwillia.ms>
2014-07-23 19:32:33 +0000

    Only dashboard admins can access formage urls

--- app/system-modules/db-admin/index.js
+++ app/system-modules/db-admin/index.js
@@ -3,10 +3,13 @@ var formage = require('formage');
 var models = require('./model');
 var Common = require(global.__commonModule);
 var conf = Common.conf;
+var mid = Common.mid;
 var fields = require('./lib/fields');
 var widgets = require('./lib/widgets');
 
 var app = Common.app;
+
+app.use('/panel', mid.restrictTo('dashboard-administrators'));
 
 formage.init(app, express, models, {
   title: 'OpenMRS ID Database Admin',
