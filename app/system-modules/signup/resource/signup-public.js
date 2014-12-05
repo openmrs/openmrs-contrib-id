18b693e0781532cbec1af26d57ac76160303ff05
Stephanie Ng <st3ph.n@outlook.com>
2014-12-05 16:13:05 -0500

    Update signup-public.js

--- app/system-modules/signup/resource/signup-public.js
+++ app/system-modules/signup/resource/signup-public.js
@@ -1,7 +1,11 @@
-(function() {
-    this.disguise = function scrambleFields(name, spin) {
-        var text = name + SPINNER;
-        var hash = CryptoJS.MD5(text);
-        return hash.toString(CryptoJS.enc.hex);
-    }
+ (function() {
+this.disguise = function scrambleFields(name, spin) {
+var text = name + SPINNER;
+var hash = CryptoJS.MD5(text);
+return hash.toString(CryptoJS.enc.hex);
+}
+
+$(document).ready(function() {
+$('.field input[placeholder=Country]').parent().css('display', 'none')
+})
 })()
