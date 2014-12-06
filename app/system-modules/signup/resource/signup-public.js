cb0880213ff0fa8c61d818c0e911333a14cea8ed
Stephanie Ng <st3ph.n@outlook.com>
2014-12-06 21:58:39 +0000

    GCI Task V2

--- app/system-modules/signup/resource/signup-public.js
+++ app/system-modules/signup/resource/signup-public.js
@@ -1,11 +1,10 @@
 (function() {
-    this.disguise = function scrambleFields(name, spin) {
-        var text = name + SPINNER;
-        var hash = CryptoJS.MD5(text);
-        return hash.toString(CryptoJS.enc.hex);
-    }
-
-    $(document).ready(function() {
-      $('.field input[placeholder=Country]').parent().css('display', 'none')
-    })
+this.disguise = function scrambleFields(name, spin) {
+var text = name + SPINNER;
+var hash = CryptoJS.MD5(text);
+return hash.toString(CryptoJS.enc.hex);
+}
+$(document).ready(function() {
+$('.field input[placeholder=Country]').parent().css('display', 'none')
+})
 })()
