2fde4b0aaff56c81bc1fd52615093a0d9572baff
Ply_py <me@plypy.com>
2015-06-18 22:00:46 +0800

    remove http request to old Answer site

--- app/routes/main.js
+++ app/routes/main.js
@@ -10,21 +10,22 @@ app.get('/', function(req, res, next) {
   if (!req.session.user) {
     return next();
   }
+  res.locals.osqaUser = false;
+  res.render('root');
+  // https.get({
+  //   host: 'answers.openmrs.org',
+  //   path: '/users/' + req.session.user.uid
+  // }, function(response) {
+  //   if (response.statusCode === 200) {
+  //     res.locals({
+  //       osqaUser: true
+  //     });
+  //   } else {
+  //     res.locals({
+  //       osqaUser: false
+  //     });
+  //   }
 
-  https.get({
-    host: 'answers.openmrs.org',
-    path: '/users/' + req.session.user.uid
-  }, function(response) {
-    if (response.statusCode === 200) {
-      res.locals({
-        osqaUser: true
-      });
-    } else {
-      res.locals({
-        osqaUser: false
-      });
-    }
-
-    res.render('root');
-  });
+  //   res.render('root');
+  // });
 });
