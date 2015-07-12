049638840a761dd366631bf1506de69a8e1899f5
Ply_py <me@plypy.com>
2015-07-12 11:42:05 +0800

    adjust middleware

--- app/system-modules/admin/lib/admin.js
+++ app/system-modules/admin/lib/admin.js
@@ -31,32 +31,31 @@ exports.addModulePage = function(name, url) {
     url: url
   });
 };
+
 // middleware, restricts to admin group, adds user-nav, css, and sidebar to any page
-exports.useSidebar = [mid.restrictTo('dashboard-administrators'),
-  function(req, res, next) {
-    var sidebar = res.locals.sidebar || [], // get current sidebar and params
-      params = res.locals.sidebarParams || {},
-      head = res.locals.headAppend || '';
+var addSidebar = function(req, res, next) {
+  var sidebar = res.locals.sidebar || [], // get current sidebar and params
+    params = res.locals.sidebarParams || {},
+    head = res.locals.headAppend || '';
 
-    // merge admin-sidebar with current
-    res.locals.sidebar = sidebar.concat(__dirname + '/../views/sidebar-admin');
-    params[__dirname + '/../views/sidebar-admin'] = {
-      className: 'box',
-      locals: {
-        pages: modulePages,
-        reqUrl: req.url
-      }
-    };
-    res.locals.sidebarParams = params;
+  // merge admin-sidebar with current
+  res.locals.sidebar = sidebar.concat(__dirname + '/../views/sidebar-admin');
+  params[__dirname + '/../views/sidebar-admin'] = {
+    className: 'box',
+    locals: {
+      pages: modulePages,
+      reqUrl: req.url
+    }
+  };
+  res.locals.sidebarParams = params;
 
-    // merge admin.css with headAppend
-    res.locals.headAppend = head + '<link rel="stylesheet" href="/admin/resource/admin.css">';
+  // merge admin.css with headAppend
+  res.locals.headAppend = head + '<link rel="stylesheet" href="/admin/resource/admin.css">';
 
-    // set view name to admin for user-nav
-    res.locals.name = 'admin';
-    next();
-  }
-];
+  // set view name to admin for user-nav
+  res.locals.name = 'admin';
+  next();
+}
 
 // expose in the common module
 Common.module.admin = exports;
@@ -66,7 +65,9 @@ Common.module.admin = exports;
 ROUTES
 ======
 */
-app.get('/admin', mid.restrictTo('dashboard-administrators'), exports.useSidebar, function(req, res, next) {
+app.get('/admin', mid.restrictTo('dashboard-administrators'), addSidebar,
+  function(req, res, next) {
+
   res.render(__dirname + '/../views/admin');
 });
 exports.addModulePage('Welcome', '/admin'); // add this page to the list
@@ -80,6 +81,8 @@ app.get('/admin/resource/*', function(req, res, next) {
 
   // transmit the file
   res.sendfile(file, function(err) {
-    if (err) return next(err);
+    if (err) {
+      return next(err);
+    }
   });
 });
