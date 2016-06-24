c9a16e8d37b8018ef3c1ef3df5c7b8c2fb4e3456
Robert O'Connor <rob@oconnor.ninja>
2016-06-24 17:28:29 -0400

    Favor nvm over globally installed node
    No release will occur from this commit

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 # start.sh and stop.sh are extremely rudimentary; you're almost better off not using them
 
 touch logs/app.pid
-NODE_PATH="/usr/local/lib/node_modules/" NODE_ENV=production nohup node app/app.js&
+NODE_ENV=production nohup node app/app.js&
 NODEPID= pgrep node | tee logs/app.pid
