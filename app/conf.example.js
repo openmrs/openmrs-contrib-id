c9bf9905a848821cbf2996cd6de323ecd25cfdee
Robert O'Connor <rob@oconnor.ninja>
2017-02-06 03:53:06 -0500

    Update the email regex to handle more TLDs

--- app/conf.example.js
+++ app/conf.example.js
@@ -88,7 +88,7 @@ module.exports = {
 		// Email settings
 
 		"validation": {
-			"emailRegex": /^[A-Za-z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+			"emailRegex": /^[A-Za-z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,10}$/i,
 		},
 		"smtp": {
 			"host": process.env.MAIL_HOST || "localhost",
