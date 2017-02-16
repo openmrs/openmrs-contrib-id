6ba6e9e12338cb8028547931b30347388ffda745
Robert O'Connor <rob@oconnor.ninja>
2017-02-16 02:23:45 -0500

    Change ownership of the logs directory

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,7 +4,8 @@ set -e
 
 # allow the container to be started with `--user`
 if [[ "$1" == npm ]] && [ "$(id -u)" = '0' ]; then
-		chown -R dashboard:dashboard /opt/id/app/user-modules/openmrs-contrib-id-globalnavbar/data/
-	  exec gosu dashboard "$BASH_SOURCE" "$@"
+    chown -R dashboard:dashboard /opt/id/app/user-modules/openmrs-contrib-id-globalnavbar/data/
+    chown -R dashboard:dashboard /opt/id/logs
+    exec gosu dashboard "$BASH_SOURCE" "$@"
 fi
 exec "$@"
