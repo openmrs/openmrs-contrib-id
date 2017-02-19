5989b98cf6f460883cc6da54d9fd5a09f72698b4
Robert O'Connor <rob@oconnor.ninja>
2017-02-19 04:55:27 -0500

    Remove the start/stop shell scripts as they are no longer necessary

--- start.sh
+++
@@ -1,5 +0,0 @@
-# start.sh and stop.sh are extremely rudimentary; you're almost better off not using them
-
-touch logs/app.pid
-NODE_ENV=production nohup node app/app.js&
-NODEPID= pgrep node | tee logs/app.pid
