31500e66dd4d7b263e459e8c70ed652bb9f21886
Robert O'Connor <rob@oconnor.ninja>
2017-02-24 20:24:10 -0500

    Update for node 6; add travis config; beautify

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 
 exports = module.exports = app => {
-	// Load strategies
-	require('./lib/discourse')(app);
-	require('./lib/deskcom')(app);
-	require('./lib/atlas')(app);
+    // Load strategies
+    require('./lib/discourse')(app);
+    require('./lib/deskcom')(app);
+    require('./lib/atlas')(app);
 };
--- tests/multipass.js
+++ tests/multipass.js
@@ -9,37 +9,37 @@ const expect = require('chai').expect;
 const apiKey = 'whitewizard';
 const siteKey = 'gandalf';
 const data = {
-	'foo': 'bar',
-	'woo': 'hoo'
+    'foo': 'bar',
+    'woo': 'hoo'
 };
 
 const crack = (multipass, signature) => {
-	const checkSignature = crypto.createHmac('sha1', apiKey)
-		.update(multipass)
-		.digest('base64');
-	if (signature !== checkSignature) {
-		return false;
-	}
+    const checkSignature = crypto.createHmac('sha1', apiKey)
+        .update(multipass)
+        .digest('base64');
+    if (signature !== checkSignature) {
+        return false;
+    }
 
-	const tmp = new Buffer(multipass, 'base64');
-	const iv = tmp.slice(0, 16);
-	multipass = tmp.slice(16);
-	const key = crypto.createHash('sha1')
-		.update(apiKey + siteKey)
-		.digest('binary').substring(0, 16);
+    const tmp = new Buffer(multipass, 'base64');
+    const iv = tmp.slice(0, 16);
+    multipass = tmp.slice(16);
+    const key = crypto.createHash('sha1')
+        .update(apiKey + siteKey)
+        .digest().slice(0, 16);
 
-	const decipher = crypto.createDecipheriv('aes-128-cbc', key, iv);
-	let result = decipher.update(multipass, 'buffer', 'utf8');
-	result += decipher.final('utf8');
-	return result;
+    const decipher = crypto.createDecipheriv('aes-128-cbc', key, iv);
+    let result = decipher.update(multipass, 'buffer', 'utf8');
+    result += decipher.final('utf8');
+    return result;
 };
 
 describe('multipass encrypting', () => {
-	it('should correctly encode the info', done => {
-		const tdata = JSON.stringify(data);
-		let tmp = multi(tdata, apiKey, siteKey);
-		tmp = crack(tmp.multipass, tmp.signature);
-		expect(tmp).to.equal(tdata);
-		done();
-	});
+    it('should correctly encode the info', done => {
+        const tdata = JSON.stringify(data);
+        let tmp = multi(tdata, apiKey, siteKey);
+        tmp = crack(tmp.multipass, tmp.signature);
+        expect(tmp).to.equal(tdata);
+        done();
+    });
 });
