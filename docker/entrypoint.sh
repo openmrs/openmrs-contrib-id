791d1c3d0ca44afc8dde5fb291ed98612112dc03
Robert O'Connor <rob@oconnor.ninja>
2017-05-08 17:05:06 -0400

    docker entry point script: use yarn vs npm

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 
 # allow the container to be started with `--user`
-if [[ "$1" == npm ]] && [ "$(id -u)" = '0' ]; then
+if [[ "$1" == "yarn" ]] && [ "$(id -u)" = '0' ]; then
     chown -R dashboard:dashboard /opt/id/app/user-modules/openmrs-contrib-id-globalnavbar/data/
     chown -R dashboard:dashboard /opt/id/logs
     exec gosu dashboard "$BASH_SOURCE" "$@"
