9ec326277bfdb86b2ae283ac876efc660fd05b4e
Robert O'Connor <rob@oconnor.ninja>
2017-05-28 23:28:16 -0400

    beautify

--- conf.example.js
+++ conf.example.js
@@ -1,24 +1,24 @@
 module.exports = {
-    // valid JSON
-    "strategies": {
+	// valid JSON
+	"strategies": {
 
-        "discourse": {
-            "secret": process.env.DISCOURSE_SECRET,
-            "nonceURL": process.env.DISCOURSE_NONCE_URL,
-            "returnURL": process.env.DISCOURSE_RETURN_URL,
-        },
-        "deskcom": {
-            "apiKey": process.env.DESKCOM_API_KEY,
-            "siteKey": process.env.DESKCOM_SITE_KEY,
-            "expiry": process.env.DESKCOM_EXPIRY,
-            "returnURL": process.env.DESKCOM_RETURN_URL,
-        },
-        "atlas": {
-            "apiKey": process.env.ATLAS_API_KEY,
-            "siteKey": process.env.ATLAS_SITE_KEY,
-            "expiry": process.env.ATLAS_EXPIRY,
-            "returnURL": process.env.ATLAS_RETURN_URL,
-        },
+		"discourse": {
+			"secret": process.env.DISCOURSE_SECRET,
+			"nonceURL": process.env.DISCOURSE_NONCE_URL,
+			"returnURL": process.env.DISCOURSE_RETURN_URL,
+		},
+		"deskcom": {
+			"apiKey": process.env.DESKCOM_API_KEY,
+			"siteKey": process.env.DESKCOM_SITE_KEY,
+			"expiry": process.env.DESKCOM_EXPIRY,
+			"returnURL": process.env.DESKCOM_RETURN_URL,
+		},
+		"atlas": {
+			"apiKey": process.env.ATLAS_API_KEY,
+			"siteKey": process.env.ATLAS_SITE_KEY,
+			"expiry": process.env.ATLAS_EXPIRY,
+			"returnURL": process.env.ATLAS_RETURN_URL,
+		},
 
-    },
+	},
 };
